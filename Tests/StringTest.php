3766c15c98ba0fedfbcb254774e463a191eb9f18
Victor Passapera <vpassapera@gmail.com>
2015-01-25 14:46:05 -0800

    Update StringTest.php

--- Tests/StringTest.php
+++ Tests/StringTest.php
@@ -12,6 +12,7 @@ class StringTest extends \PHPUnit_Framework_TestCase
 {
     const STRING_SINGULAR = "syllabus";
     const STRING_PLURAL   = "syllabi";
+    const LOREM_IPSUM     = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
 
     public function testSingular()
     {
@@ -23,5 +24,67 @@ class StringTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals((string) (new String(self::STRING_SINGULAR))->pluralize(), self::STRING_PLURAL);
     }
 
+    public function testSubStrUntil()
+    {
+        $this->assertEquals(
+            'Lorem ipsum dolor sit amet',
+            (string) String::create($this->loremIpsum)->subStrUntil(',', true)
+        );
+        $this->assertEquals(
+            'Lorem ipsum dolor sit amet,',
+            (string) String::create($this->loremIpsum)->subStrUntil(',')
+        );
+    }
+
+    public function testSubStrAfter()
+    {
+        $this->assertEquals(
+            'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            (string) String::create($this->loremIpsum)->subStrAfter('elit, ', true)
+        );
+        $this->assertEquals(
+            'elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            (string) String::create($this->loremIpsum)->subStrAfter('elit, ')
+        );
+    }
+
+    public function testSubStrFromTo()
+    {
+        $this->assertEquals(
+            'consectetur adipiscing elit',
+            (string) String::create($this->loremIpsum)->subStrFromTo('amet, ', ', sed', true, true)
+        );
+        $this->assertEquals(
+            'amet, consectetur adipiscing elit',
+            (string) String::create($this->loremIpsum)->subStrFromTo('amet, ', ', sed', false, true)
+        );
+        $this->assertEquals(
+            'consectetur adipiscing elit, sed',
+            (string) String::create($this->loremIpsum)->subStrFromTo('amet, ', ', sed', true, false)
+        );
+        $this->assertEquals(
+            'amet, consectetur adipiscing elit, sed',
+            (string) String::create($this->loremIpsum)->subStrFromTo('amet, ', ', sed')
+        );
+    }
+
+    public function testStrpos()
+    {
+        $this->assertEquals(6, String::create($this->loremIpsum)->strpos('ipsum'));
+    }
+
+    public function testAddSpaces()
+    {
+        $str = "    Hello world!" . "\n" . "    Foo bar!";
+        $this->assertEquals($str, (string) String::create("Hello world!\nFoo bar!")->addSpaces(4, null, true));
+        $str = "    Foo Test.";
+        $this->assertEquals($str, (string) String::create("Foo Test.")->addSpaces());
+    }
+
+    public function testGetSpaceSize()
+    {
+        $str = "\n" . '    $test';
+        $this->assertEquals(4, (int) String::create($str)->getSpaceSize());
+    }
 }
 
